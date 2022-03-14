93f425baef899b1c42452b237f06d1214ce06fe3
John Margaronis <john@johnmargaronis.com>
2022-03-14 14:54:26 -0500

    Okeh final push

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -5,19 +5,43 @@ async function httpGetPlanets() {
   return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(launch)
+    });
+  } catch (err) {
+    console.log(err);
+    return {
+      ok: false
+    };
+  }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'DELETE'
+    });
+  } catch (err) {
+    console.log(err);
+    return {
+      ok: false
+    };
+  }
 }
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
